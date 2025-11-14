abde0e5669e79814f81db841940dc743a5332449
mcapo <miguelorestecoding@NT-MPF-209633.localdomain>
2025-11-14 14:59:57 +0000

    Tailwind funcionando

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Devstagram - @yield('titulo')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 
 <body>
