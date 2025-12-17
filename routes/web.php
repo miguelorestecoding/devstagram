b1c024350f74d90922d65ac5ba6f027ab1ef7c99
mcapo <miguelorestecoding@NT-MPF-209633.localdomain>
2025-12-17 14:53:49 +0000

    Video 52 completo

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,3 @@ Route::get('/', function () {
     return view('principal');
 });
 
-Route::get('/nosotros', function () {
-    return view('nosotros');
-});
-Route::get('/phppractice', function () {
-    return view('phppractice');
-});
-
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::post('/register', [RegisterController::class, 'store']);
-
-Route::get('/tienda', function () {
-    return view('tienda');
-});
-
-Route::get('/test', function () {
-    return 'Funciona';
-});
